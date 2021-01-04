8e99a2c4aac6b85a52a26a0da6f7d3343fc216ae
Nathan Pereira <7155433-nathan.pereira@users.noreply.gitlab.com>
2021-01-03 21:22:13 -0300

    Implementação CEP api

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-    <p>Login Criar</p>
-  </div>
+  <section>
+    <h2>Cadastre-se!</h2>
+    <transition mode="out-in">
+      <button v-if="!criar" @click="criar=true" class="btn criar">Criar Conta</button>
+      <UsuarioForm v-else>
+        <button class="btn">Cadastro</button>
+      </UsuarioForm>
+    </transition>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from "@/components/UsuarioForm.vue"
 export default {
-  name: "LoginCriar"
+  name: "LoginCriar",
+  components: {
+    UsuarioForm
+  },
+  data() {
+    return {
+      criar: false,
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
